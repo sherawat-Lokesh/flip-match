aeb865f2ba3a94c1b1617e8eb558deb922272606
sherawat <sherawat@sherawats-MacBook-Air.local>
2023-01-13 15:13:13 -0800

    nothing changed

--- flip.js
+++ flip.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const img=['playing cards/B.SVG','playing cards/A.SVG','playing cards/D.SVG','playing cards/E.svg','playing cards/A.SVG','playing cards/C.SVG','playing cards/F.SVG','playing cards/B.SVG','playing cards/D.SVG','playing cards/C.SVG','playing cards/F.SVG','playing cards/E.svg']
+const img=['playing cards/B.jpeg','playing cards/A.jpeg','playing cards/D.jpeg','playing cards/E.jpeg','playing cards/A.jpeg','playing cards/C.jpeg','playing cards/F.jpeg','playing cards/B.jpeg','playing cards/D.jpeg','playing cards/C.jpeg','playing cards/F.jpeg','playing cards/E.jpeg']
 
 function createElement(i){
     const hiddenIMg=document.createElement('img');
@@ -19,33 +19,44 @@ let score=0
 function topImgGradiantClick(){
     const img2=img.sort((a,b)=>0.5-Math.random());
     const allImg=document.querySelectorAll('.flip')
-        allImg.forEach(element=>{
+
+        allImg.forEach(element=>{run(element)})
+
+        function run(element){
+            console.log(checkIfMatch)
             element.addEventListener('click',(e)=>{
-                e.preventDefault()
+
                 console.log(img2[e.target.id])
                 checkIfMatch.push({name:img2[e.target.id],id:e.target.id})
-                console.log(checkIfMatch)
+        
                 e.target.src=img2[e.target.id];
         
                 if(checkIfMatch.length === 2){
                     let a=checkIfMatch[0],b=checkIfMatch[1];
                         if(a.name!==b.name){
                             console.log('matched')
-                            setTimeout(()=>{
-                                document.getElementById(`${+checkIfMatch[0].id}`).src=`playing cards/hidden.svg`;
-                                document.getElementById(`${+checkIfMatch[1].id}`).src=`playing cards/hidden.svg`;
-                                checkIfMatch=[]
-                            },500)
+                                setTimeout(()=>{
+                                    document.getElementById(`${+checkIfMatch[0].id}`).src=`playing cards/hidden.svg`;
+                                    document.getElementById(`${+checkIfMatch[1].id}`).src=`playing cards/hidden.svg`;
+                                    checkIfMatch=[]
+                                },200)
+        
                         }
                         if(a.name === b.name){
+                            document.getElementById(`${+checkIfMatch[0].id}`).removeEventListener('click',run)
+                            document.getElementById(`${+checkIfMatch[1].id}`).removeEventListener('click',run)
                             score +=1
                             console.log(score)
                             checkIfMatch=[]
                         }
-                        if(score===6){
+                        if(score => 6){
+                            
 
                         }
                 }
         })
-    })
+        }
 }topImgGradiantClick()
+
+
+
